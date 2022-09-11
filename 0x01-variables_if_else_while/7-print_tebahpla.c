51bf88330ca36b33a40a647ac65ecbfe770fbd58
erhis daniel <66309753+lienadochuko@users.noreply.github.com>
2022-09-11 23:27:51 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,16 +3,17 @@
 /**
  * main - print lowercase reverse
  *
- * Return - Always 0 (success)
+ * Return - Always 0 (Success)
  */
 int main(void)
 {
+	char alp[26] = "zyxwvutsrqponmlkjihgfedcba";
 	int ch;
 
-	for (ch = 26; ch > 0; ch--)
+	for (ch = 0; ch > 26; ch++)
 	{
-		putchar(ch);
+		putchar(alp[ch]);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
