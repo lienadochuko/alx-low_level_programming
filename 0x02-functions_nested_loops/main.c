4ca0554652d9dba4422040a20a48179f55fd6f6b
lienadochuko <you@example.comcsdayhgjklk.s>
2022-09-13 12:20:02 -0700

    header and .h file

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar with new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alp[] = {45, 112, 117, 116, 99, 104, 97, 114};
+	int i, z;
+
+	z = sizeof(alp) / sizeof(int);
+	for (i = 0; i < z; i++)
+	{
+		_putchar(alp[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
