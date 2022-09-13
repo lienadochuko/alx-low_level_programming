6a3781678caa3973d3c98d9c0a669c97f1b714f5
lienadochuko <you@example.comcsdayhgjklk.s>
2022-09-13 12:27:14 -0700

    header and .h file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	int alp[] = {45, 112, 117, 116, 99, 104, 97, 114};
+	int str[] = {45, 112, 117, 116, 99, 104, 97, 114};
 	int i, z;
-	
-	z = sizeof(alp) / sizeof(int);
+
+	z = sizeof(str) / sizeof(int);
 	for (i = 0; i < z; i++)
 	{
-		_putchar(alp[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 	return (0);
