cdb83e9a8747c47146bd9066ee0edb87e8ef5f31
lienadochuko <you@example.comcsdayhgjklk.s>
2022-09-13 13:48:19 -0700

    p

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,9 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	char alp[8] = "_putchar";
 	int ch;
-=======
-	_putchar('_');
-	_putchar('putchar');
-	_putchar('\n');
->>>>>>> 6183bae03e2b3404e0b54221749dc5cc12678505
-
+	
 	for (ch = 0; ch < 8; ch++)
 	{
 		_putchar(alp[ch]);
