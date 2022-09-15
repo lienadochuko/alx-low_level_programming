44d3137dbeb4a500130454d844f747dd681a88dc
lienadochuko <you@example.comcsdayhgjklk.s>
2022-09-15 16:09:05 -0700

    Write a function that prints the numbers, from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "mian.h"
+
+/**
+ * print_numbers - Prints the numbers followed by new line.
+ * @c: contains numbers.
+ * Return: numbers.
+ */
+void print_numbers(void)
+{
+	if (c >= 47 && c <= 57)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+}
