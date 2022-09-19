a3a7e954f66ef83dc2674d54dbfcf8832daa63f1
lienadochuko <you@example.comcsdayhgjklk.s>
2022-09-19 11:39:08 -0700

    Write a function that prints a string, followed by a new line, to stdout.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,10 +9,14 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] == '\0')
+	while (i >= 0)
 	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
 		_putchar(str[i]);
-		_putchar('\n');
 		i++;
 	}
 }
