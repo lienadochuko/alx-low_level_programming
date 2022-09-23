c08fe140bd2c00b6467242de59a94cf38bce1cfe
lienadochuko <you@example.comcsdayhgjklk.s>
2022-09-23 14:41:17 -0700

    Write a function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,18 +7,13 @@
  */
 void print_number(int n)
 {
-	unsigned int m;
-
 	if (n < 0)
 	{
-		m = n;
 		_putchar(45);
-		_putchar(m);
+		_putchar(n);
 	}
 	else
 	{
-		m = n;
-		_putchar(m);
+		_putchar(n);
 	}
-	_putchar('\n');
 }
