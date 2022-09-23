7ac48c2f62f91c2cb1f6e4e3494098feed9bcf58
erhis daniel <66309753+lienadochuko@users.noreply.github.com>
2022-09-24 00:34:43 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifdef _MAIN_H_
-#define _MAIN_H_
+#ifdef _main_h_
+#define _main_h_
 
 int _putchar(char);
 char *_strcat(char *dest, char *src);
@@ -11,8 +11,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
 
 #endif
