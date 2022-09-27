1e185774fb182788f3fa969148adca8311c41613
lienadochuko <you@example.comcsdayhgjklk.s>
2022-09-27 15:37:49 -0700

    Write a function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - Fills first n bytes of memory.
- * @s: A pointer to the memory area to filled.
- * @b: The character to fill the memory area.
- * @n: The number of bytes to be filled.
+ * _memset - fills the first n bytes of ,
+ * the memory.
+ * @s: a pointer to the memory area to filled.
+ * @b: the character to fill the memory area.
+ * @n: the number of bytes to be filled.
  *
- * Return: A pointer to the filled memory.
+ * Return: a pointer to the filled memory.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int ic;
 	
 	for (i = 0; i < n; i++)
-		*(s + i) = b;
+		*(s + i) =  b;
 	
 	return (s);
 }
