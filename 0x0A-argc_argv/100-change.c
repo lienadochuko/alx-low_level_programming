586a933c68c14c09812e83a315934dd1b1904171
lienadochuko <you@example.comcsdayhgjklk.s>
2022-10-04 04:12:40 -0700

    Write a program that prints the minimum number of coins to make change for an amount of money.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - prints the minimum number of coins to make change for 
+ * main - prints the minimum number of coins to make change for.
  * an amount of money.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
